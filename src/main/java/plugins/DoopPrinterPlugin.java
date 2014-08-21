d18d1ccced8bcbf65d586fec7ac4caff3e3a53ce
Aggelos Biboudis <biboudis@gmail.com>
2014-08-21 15:27:08 +0300

    Initial Commit.

---
+++ src/main/java/plugins/DoopPrinterPlugin.java
@@ -0,0 +1,17 @@
+package plugins;
+
+import com.sun.source.util.JavacTask;
+import com.sun.source.util.Plugin;
+
+public class DoopPrinterPlugin implements Plugin {
+
+	@Override
+	public String getName() {
+		return "DoopPrinterPlugin";
+	}
+
+	@Override
+	public void init(JavacTask task, String... arg1) {
+		task.addTaskListener(new DoopPrinterTaskListener(task));
+	}
+}
---
+++ src/main/java/plugins/DoopPrinterTaskListener.java
@@ -0,0 +1,39 @@
+package plugins;
+
+import com.sun.source.util.JavacTask;
+import com.sun.source.util.TaskEvent;
+import com.sun.source.util.TaskListener;
+import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.Pretty;
+import visitors.DoopPrinter;
+
+import java.io.StringWriter;
+
+public class DoopPrinterTaskListener implements TaskListener {
+	private JavacTask task;
+	
+	public DoopPrinterTaskListener(JavacTask javactask) {
+		task = javactask;
+	}
+
+	@Override
+	public void finished(TaskEvent arg0) {
+		System.out.println("# Task Kind: " + arg0.getKind());
+
+		if (arg0.getKind().equals(TaskEvent.Kind.ANALYZE)) {
+
+            JCTree tree = (JCTree) arg0.getCompilationUnit();
+
+            StringWriter s = new StringWriter();
+
+			tree.accept(new DoopPrinter(s, false));
+
+            System.out.println(s.toString());
+		}
+
+	}
+
+	@Override
+	public void started(TaskEvent arg0) {
+	}
+}
