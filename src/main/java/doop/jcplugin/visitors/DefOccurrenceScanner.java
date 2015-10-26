320706518385d53976fd4e7e399cd217d89376e7
Anastasios Antoniadis <anastasios.antoniadis@gmail.com>
2015-10-26 23:40:33 +0200

    Add field occurrences - need to handle this variable

--- src/main/java/doop/jcplugin/visitors/DefOccurrenceScanner.java
+++ src/main/java/doop/jcplugin/visitors/DefOccurrenceScanner.java
@@ -3,10 +3,8 @@ package doop.jcplugin.visitors;
 import com.sun.source.tree.LineMap;
 import com.sun.tools.javac.tree.JCTree.JCIdent;
 import doop.jcplugin.util.SourceFileReport;
-import doop.persistent.elements.Occurrence;
-import doop.persistent.elements.Position;
-import doop.persistent.elements.Symbol;
-import doop.persistent.elements.Variable;
+import doop.persistent.elements.*;
+
 import java.util.Map;
 import static doop.persistent.elements.OccurrenceType.WRITE;
 
@@ -43,6 +41,27 @@ public class DefOccurrenceScanner extends OccurrenceScanner {
                 System.out.println("Reported variable occurrence: (def)" + var.getName());
                 System.out.println("Source file name: " + var.getSourceFileName());
                 System.out.println("Position: " + var.getPosition().getStartLine());
+            }
+            else if (this.varSymbolMap.containsKey(tree.sym.hashCode()) && this.varSymbolMap.get(tree.sym.hashCode()) instanceof Field) {
+
+                Position position = new Position(this.lineMap.getLineNumber(tree.pos),
+                        this.lineMap.getColumnNumber(tree.pos),
+                        this.lineMap.getColumnNumber(tree.pos + tree.sym.name.toString().length()));
+
+
+                Occurrence occurrence = new Occurrence(position,
+                        this.sourceFileName,
+                        this.varSymbolMap.get(tree.sym.hashCode()).getId(),
+                        WRITE);
+
+                SourceFileReport.occurrenceList.add(occurrence);
+                Field field = (Field) this.varSymbolMap.get(tree.sym.hashCode());
+                System.out.println("Reported field occurrence: (def)" + field.getName());
+                System.out.println("Source file name: " + field.getSourceFileName());
+                System.out.println("Position: " + field.getPosition().getStartLine());
+            }
+            else {
+                System.out.println("*** Occurrence of other identifier: " + tree.sym.getQualifiedName());
             }
         }
     }
--- src/main/java/doop/jcplugin/visitors/UseOccurrenceScanner.java
+++ src/main/java/doop/jcplugin/visitors/UseOccurrenceScanner.java
@@ -3,12 +3,11 @@ package doop.jcplugin.visitors;
 import com.sun.source.tree.LineMap;
 import com.sun.tools.javac.tree.JCTree;
 import doop.jcplugin.util.SourceFileReport;
-import doop.persistent.elements.Occurrence;
-import doop.persistent.elements.Position;
-import doop.persistent.elements.Symbol;
-import doop.persistent.elements.Variable;
+import doop.persistent.elements.*;
+
 import java.util.Map;
 import static doop.persistent.elements.OccurrenceType.READ;
+import static doop.persistent.elements.OccurrenceType.WRITE;
 
 /**
  * Created by anantoni on 22/10/2015.
@@ -39,6 +38,27 @@ public class UseOccurrenceScanner extends OccurrenceScanner {
                 System.out.println("Reported variable occurrence: (def)" + var.getName());
                 System.out.println("Source file name: " + var.getSourceFileName());
                 System.out.println("Position: " + var.getPosition().getStartLine());
+            }
+            else if (this.varSymbolMap.containsKey(tree.sym.hashCode()) && this.varSymbolMap.get(tree.sym.hashCode()) instanceof Field) {
+
+                Position position = new Position(this.lineMap.getLineNumber(tree.pos),
+                        this.lineMap.getColumnNumber(tree.pos),
+                        this.lineMap.getColumnNumber(tree.pos + tree.sym.name.toString().length()));
+
+
+                Occurrence occurrence = new Occurrence(position,
+                        this.sourceFileName,
+                        this.varSymbolMap.get(tree.sym.hashCode()).getId(),
+                        READ);
+
+                SourceFileReport.occurrenceList.add(occurrence);
+                Field field = (Field) this.varSymbolMap.get(tree.sym.hashCode());
+                System.out.println("Reported field occurrence: (def)" + field.getName());
+                System.out.println("Source file name: " + field.getSourceFileName());
+                System.out.println("Position: " + field.getPosition().getStartLine());
+            }
+            else {
+                System.out.println("*** Occurrence of other identifier: " + tree.sym.getQualifiedName());
             }
         }
     }
